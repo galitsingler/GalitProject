6f5cdb050786310de39630c5f63421aafcaedfdb
Galit Singler <galit.singler3006@gmail.com>
2020-03-01 09:44:34 +0200

    asdfghjk

--- Galitproject/Galitproject/views.py
+++ Galitproject/Galitproject/views.py
@@ -6,12 +6,50 @@ from datetime import datetime
 from flask import render_template
 from Galitproject import app
 
+
+from datetime import datetime
+from flask import render_template
+
+from Galitproject.models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+from Galitproject.models.QueryFormStructure import QueryFormStructure 
+from Galitproject.models.QueryFormStructure import LoginFormStructure 
+from Galitproject.models.QueryFormStructure import UserRegistrationFormStructure 
+
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
     return render_template(
-        'index.html',
+        'home.html',
         title='Home Page',
         year=datetime.now().year,
     )
@@ -23,7 +61,7 @@ def contact():
         'contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
+        message='my contact page.'
     )
 
 @app.route('/about')
@@ -33,14 +71,73 @@ def about():
         'about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='about page.'
     )
-@app.route('/query')
-def query():
+
+@app.route('/DataModel')
+def DataModel():
     """Renders the about page."""
     return render_template(
-        'query.html',
-        title='Query',
+        'DataModel.html',
+        title='Data Model',
         year=datetime.now().year,
         message='Your application description page.'
     )
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+# -------------------------------------------------------
+# Login page
+# This page is the filter before the data analysis
+# -------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+            #return redirect('<were to go if login is good!')
+        else:
+            flash('Error in - Username and/or password')
+
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+@app.route('/dataset')
+def dataset():
+    df = pd.read_excel('Galitproject/static/data/olim.xls')
+    raw_data_table = df.to_html(classes='table table-hover', max_rows=8)
+
+    return render_template(
+        'dataset.html',
+        title='Dataset Description',
+        message='Dataset description page',
+        raw_data_table=raw_data_table,
+        year=datetime.now().year
+        )
